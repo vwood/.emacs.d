fb9cb316d55801fbc795a61b1006a1ff6f7406f6
VWood <vwood.org@gmail.com>
2012-04-18 14:32:29 +1200

    Move solarized to be an el-get dependency

--- init.el
+++ init.el
@@ -334,7 +334,7 @@
                     (end-of-buffer) 
                     (eval-print-last-sexp)))))
 (setq el-get-git-shallow-clone t)
-(el-get 'sync '(workgroups graphviz-dot-mode markdown-mode color-theme))
+(el-get 'sync '(workgroups graphviz-dot-mode markdown-mode color-theme color-theme-solarized))
 
 ;; Use workgroups if available, otherwise try escreen ...
 (if (require 'workgroups nil t)
