4c36142248eefeb255ac2988fe5bba97e8961cd3
VWood <vwood.org@gmail.com>
2012-05-04 11:39:46 +1200

    Only fix *-bar modes if we have a window-system

--- init.el
+++ init.el
@@ -18,9 +18,11 @@
 (add-to-list 'load-path "~/.emacs.d")
 
 ;; Remove clutter
-(scroll-bar-mode -1)
-(tool-bar-mode -1)
-(menu-bar-mode -1)
+
+(when window-system
+  (scroll-bar-mode -1)
+  (tool-bar-mode -1)
+  (menu-bar-mode -1))
 (setq inhibit-splash-screen t)
 
 ;; Always use CommonLisp extensions
